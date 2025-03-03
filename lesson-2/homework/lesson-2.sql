ee93e24d12eab2d0341661315b36c8cb049cb0a5
Serdali <m.serdali911@gmail.com>
2025-03-03 18:53:15 +0500

    adding new file

---
+++ lesson-2/homework/lesson-2.sql
@@ -0,0 +1,71 @@
+--CREATE DATABASE hwlesson_2
+
+---1 task
+
+CREATE TABLE Salesman (salesman_id INT, name varchar(50), city varchar(50), commission float)
+INSERT INTO Salesman VALUES
+(5001, 'James Hoog', 'New York', 0.15),
+(5002, 'Nail Knite', 'Paris', 0.13),
+(5005, 'Pit Alex', 'London', 0.11),
+(5006, 'Mc Lyon', 'Paris', 0.14),
+(5007, 'Paul Adam', 'Rome', 0.13),
+(5003, 'Lauson Hen', 'San Jose', 0.12)
+
+---2 task
+CREATE TABLE Orders (ord_no INT, purch_amt FLOAT, ord_date DATE, customer_id INT, salesman_id INT)
+INSERT INTO Orders VALUES
+(70001, 150.5, '2012-10-05', 3005, 5002),
+(70009, 270.65, '2012-09-10', 3001, 5005),
+(70002, 65.26, '2012-10-05', 3002, 5001),
+(70004, 110.5, '2012-08-17', 3009, 5003),
+(70007, 948.5, '2012-09-10', 3005, 5002),
+(70005, 2400.6, '2012-07-27', 3007, 5001),
+(70008, 5760, '2012-09-10', 3002, 5001),
+(70010, 1983.43, '2012-10-10', 3004, 5006),
+(70003, 2480.4, '2012-10-10', 3009, 5003),
+(70012, 250.45, '2012-06-27', 3008, 5002),
+(70011, 75.29, '2012-08-17', 3003, 5007),
+(70013, 3045.6, '2012-04-25', 3002, 5001)
+
+SELECT ord_date, salesman_id, ord_no, purch_amt 
+FROM Orders
+
+---3 task
+CREATE TABLE Customer (customer_id INT, cust_name VARCHAR(50), city VARCHAR(50), grade INT, salesman_id INT)
+INSERT INTO Customer VALUES
+(3002, 'Nick Rimando', 'New York', 100, 5001),
+(3007, 'Brad Davis', 'New York', 200, 5001),
+(3005, 'Graham Zusi', 'California', 200, 5002),
+(3008, 'Julian Green', 'London', 300, 5002),
+(3004, 'Fabian Johnson', 'Paris', 300, 5006),
+(3009, 'Geoff Cameron', 'Berlin', 100, 5003),
+(3003, 'Jozy Altidor', 'Moscow', 200, 5007),
+(3001, 'Brad Guzan', 'London', NULL, 5005)
+
+---4 task
+CREATE TABLE nobel_win (YEAR INT, SUBJECT VARCHAR(50), WINNER VARCHAR(75), COUNTRY VARCHAR(50), CATEGORY VARCHAR(50))
+INSERT INTO nobel_win VALUES
+(1970, 'Physics', 'Hannes Alfven', 'Sweden', 'Scientist'),
+(1970, 'Physics', 'Louis Neel', 'France', 'Scientist'),
+(1970, 'Chemistry', 'Luis Federico Leloir', 'France', 'Scientist'),
+(1970, 'Physiology', 'Ulf von Euler', 'Sweden', 'Scientist'),
+(1970, 'Physiology', 'Bernard Katz', 'Germany', 'Scientist'),
+(1970, 'Literature', 'Aleksandr Solzhenitsyn', 'Russia', 'Linguist'),
+(1970, 'Economics', 'Paul Samuelson', 'USA', 'Economist'),
+(1970, 'Physiology', 'Julius Axelrod', 'USA', 'Scientist'),
+(1971, 'Physics', 'Dennis Gabor', 'Hungary', 'Scientist'),
+(1971, 'Chemistry', 'Gerhard Herzberg', 'Germany', 'Scientist'),
+(1971, 'Peace', 'Willy Brandt', 'Germany', 'Chancellor'),
+(1971, 'Literature', 'Pablo Neruda', 'Chile', 'Linguist'),
+(1971, 'Economics', 'Simon Kuznets', 'Russia', 'Economist'),
+(1978, 'Peace', 'Anwar al-Sadat', 'Egypt', 'President'),
+(1978, 'Peace', 'Menachem Begin', 'Israel', 'Prime Minister'),
+(1987, 'Chemistry', 'Donald J. Cram', 'USA', 'Scientist'),
+(1987, 'Chemistry', 'Jean-Marie Lehn', 'France', 'Scientist'),
+(1987, 'Physiology', 'Susumu Tonegawa', 'Japan', 'Scientist'),
+(1994, 'Economics', 'Reinhard Selten', 'Germany', 'Economist'),
+(1994, 'Peace', 'Yitzhak Rabin', 'Israel', 'Prime Minister'),
+(1987, 'Physics', 'Johannes Georg Bednorz', 'Germany', 'Scientist'),
+(1987, 'Literature', 'Joseph Brodsky', 'Russia', 'Linguist'),
+(1987, 'Economics', 'Robert Solow', 'USA', 'Economist'),
+(1994, 'Literature', 'Kenzaburo Oe', 'Japan', 'Linguist')
